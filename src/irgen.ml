e4fc33e0b15a870492f3a984ecb4f931d16b1c02
Daniel Garces <dg3008@columbia.edu>
2020-05-13 22:06:44 -0400

    Partial implementation of memory management

--- src/irgen.ml
+++ src/irgen.ml
@@ -146,7 +146,7 @@ let translate (globals, functions) =
   let assignMutBool_t : L.lltype = L.function_type (void_t)
     [| L.pointer_type struct_mut_bool_t; i1_t |] in
 
-  let assignMutBool : L.llvalue = L.declare_function "assignMutBool" assignMutInt_t the_module in
+  let assignMutBool : L.llvalue = L.declare_function "assignMutBool" assignMutBool_t the_module in
 
     (* Read *)
   let readMutInt_t : L.lltype = L.function_type (i32_t)
@@ -269,7 +269,7 @@ let translate (globals, functions) =
     List.fold_left function_decl StringMap.empty functions in
 
   (* Fill in the body of the given function *)
-  let build_fnction_body fdecl =
+  letbuild_fnction_body fdecl =
     let (the_function, _) = StringMap.find fdecl.sfname function_decls in
     let builder = L.builder_at_end context (L.entry_block the_function) in
 
@@ -282,7 +282,16 @@ let translate (globals, functions) =
       let add_formal m (t, n) p =
         L.set_value_name n p;
         let local = L.build_alloca (ltype_of_typ t) n builder in
-        ignore (L.build_store p local builder);
+        ignore ( match t with 
+                | A.Mut(x) -> (match x with 
+                                | A.Int -> ignore(L.build_call initMutInt [| local |] "" builder);
+                                            (L.build_call assignMutInt [| local; p |] "" builder) 
+                                | A.Float -> ignore(L.build_call initMutFloat [| local |] "" builder);
+                                            (L.build_call assignMutFloat [| local; p |] "" builder) 
+                                | A.Bool -> ignore(L.build_call initMutBool [| local |] "" builder);
+                                            (L.build_call assignMutBool [| local; p |] "" builder) 
+                                )
+                | _ -> L.build_store p local builder);
         StringMap.add n local m
 
       (* Allocate space for any locally declared variables and add the
@@ -302,6 +311,7 @@ let translate (globals, functions) =
                                     )
                     | _ -> local_var
         in StringMap.add n local_var m
+
       in
 
       let formals = List.fold_left2 add_formal StringMap.empty fdecl.sformals
@@ -314,17 +324,129 @@ let translate (globals, functions) =
     let lookup n = try StringMap.find n local_vars
       with Not_found -> StringMap.find n global_vars
     in
-
+    
     (* Construct code for an expression; return its value *)
-    let rec build_expr builder ((_, e) : sexpr) = match e with
+    let rec build_expr_args builder ((typ, e) : sexpr) = match e with
         SIntLit i  -> L.const_int i32_t i
       | SBoolLit b  -> L.const_int i1_t (if b then 1 else 0)
       | SFloatLit f -> L.const_float float_t f
       (*|SCharLit c -> L... c*)
       (*| SStrLit s -> s*)
-      | SVar s       -> L.build_load (lookup s) s builder
-      | SAssign (s, e) -> let e' = build_expr builder e in
-        ignore(L.build_store e' (lookup s) builder); e'
+      | SVar s       -> (match typ with 
+                            | A.Mut(A.Int) -> (L.build_call readMutInt [| (lookup s) |] "" builder)
+
+                            | A.Mut(A.Float) -> (L.build_call readMutFloat [| (lookup s) |] "" builder)
+                            | A.Mut(A.Bool) -> (L.build_call readMutBool [| (lookup s) |] "" builder)
+                            | _ -> L.build_load (lookup s) s builder)
+      | SAssign (s, e) -> let e' = (match (fst e) with
+                                    | A.Mut(x) when typ = A.Ref(x) -> 
+                                        (match (snd e) with 
+                                        | SVar y -> L.build_load (lookup y) y builder
+                                        | _ -> L.build_load (lookup s) s builder )
+                                    | _ -> build_expr_args builder e) in
+                            ignore (match typ with 
+                            | A.Mut(A.Int) -> (L.build_call assignMutInt [| (lookup s); e' |] "" builder)                                                                               
+                            | A.Mut(A.Float) -> (L.build_call assignMutFloat [| (lookup s); e' |] "" builder    )                                                        
+                            | A.Mut(A.Bool) -> (L.build_call assignMutBool [| (lookup s); e' |] "" builder)
+                            | A.Ref(A.Int) when fst e = A.Mut(A.Int) 
+                            -> (L.build_call assignRefIntMut [| (lookup s); e' |] "" builder)
+                            | A.Ref(A.Int) when fst e = A.Ref(A.Int) 
+                            -> (L.build_call assignRefIntRef [| (lookup s); e' |] "" builder)
+                            | A.Ref(A.Float) when fst e = A.Mut(A.Float) 
+                            -> (L.build_call assignRefFloatMut [| (lookup s); e' |] "" builder)
+                            | A.Ref(A.Float) when fst e = A.Ref(A.Float) 
+                            -> (L.build_call assignRefFloatRef [| (lookup s); e' |] "" builder)
+                            | A.Ref(A.Bool) when fst e = A.Mut(A.Bool) 
+                            -> (L.build_call assignRefBoolMut [| (lookup s); e' |] "" builder)
+                            | A.Ref(A.Bool) when fst e = A.Ref(A.Bool) 
+                            -> (L.build_call assignRefBoolRef [| (lookup s); e' |] "" builder)
+
+                            | _ -> L.build_store e' (lookup s) builder); e'
+
+      | SBinop (e1, op, e2) ->
+        let e1' = build_expr_args builder e1
+        and e2' = build_expr_args builder e2 in
+        (match op with
+           A.Add     -> L.build_add
+         | A.Sub     -> L.build_sub
+         | A.Mult    -> L.build_mul
+         | A.Div     -> L.build_sdiv
+         | A.FAdd    -> L.build_fadd
+         | A.FSub    -> L.build_fsub
+         | A.FMult   -> L.build_fmul
+         | A.FDiv    -> L.build_fdiv
+         | A.Eq      -> L.build_icmp L.Icmp.Eq
+         | A.Neq     -> L.build_icmp L.Icmp.Ne
+         | A.Lt      -> L.build_icmp L.Icmp.Slt
+         | A.Gt      -> L.build_icmp L.Icmp.Sgt
+         | A.Leq     -> L.build_icmp L.Icmp.Sle
+         | A.Geq     -> L.build_icmp L.Icmp.Sge
+         | A.FEq     -> L.build_fcmp L.Fcmp.Oeq
+         | A.FNeq    -> L.build_fcmp L.Fcmp.One
+         | A.FLt     -> L.build_fcmp L.Fcmp.Olt
+         | A.FGt     -> L.build_fcmp L.Fcmp.Ogt
+         | A.FLeq    -> L.build_fcmp L.Fcmp.Ole
+         | A.FGeq    -> L.build_fcmp L.Fcmp.Oge
+         | A.And     -> L.build_and
+         | A.Or      -> L.build_or
+         | A.Not     -> L.build_xor
+
+        ) e1' e2' "tmp" builder
+      | SCall ("print", [e]) ->
+        L.build_call printf_func [| int_format_str ; (build_expr_args builder e) |]
+          "printf" builder
+      | SCall (f, args) ->
+        let (fdef, fdecl) = StringMap.find f function_decls in
+        let llargs = List.rev (List.map (build_expr_args builder) (List.rev args)) in
+        let result = f ^ "_result" in
+        L.build_call fdef (Array.of_list llargs) result builder
+    in
+
+    (* Construct code for an expression; return its value *)
+    let rec build_expr builder ((typ, e) : sexpr) = match e with
+        SIntLit i  -> L.const_int i32_t i
+      | SBoolLit b  -> L.const_int i1_t (if b then 1 else 0)
+      | SFloatLit f -> L.const_float float_t f
+      (*|SCharLit c -> L... c*)
+      (*| SStrLit s -> s*)
+      | SVar s       -> (match typ with 
+                            | A.Mut(A.Int) -> (L.build_call readMutInt [| (lookup s) |] "" builder)
+
+                            | A.Mut(A.Float) -> (L.build_call readMutFloat [| (lookup s) |] "" builder)
+                            | A.Mut(A.Bool) -> (L.build_call readMutBool [| (lookup s) |] "" builder)
+                            | A.Ref(A.Int) -> (L.build_call readRefInt [| (lookup s) |] "" builder)
+                            | A.Ref(A.Float) -> (L.build_call readRefFloat [| (lookup s) |] "" builder)
+                            | A.Ref(A.Bool) -> (L.build_call readRefBool [| (lookup s) |] "" builder)
+
+                            | _ -> L.build_load (lookup s) s builder)
+      | SAssign (s, e) -> let e' = (match (fst e) with
+                                    | A.Mut(x) when typ = A.Ref(x) -> 
+                                        (match (snd e) with 
+                                        | SVar y -> L.build_load (lookup y) y builder
+                                        | _ -> L.build_load (lookup s) s builder )
+                                    | A.Ref(x) when typ = A.Ref(x) ->
+                                        (match (snd e) with 
+                                        | SVar y -> L.build_load (lookup y) y builder 
+                                        | _ -> L.build_load (lookup s) s builder )
+                                    | _ -> build_expr builder e) in
+                            ignore (match typ with 
+                            | A.Mut(A.Int) -> (L.build_call assignMutInt [| (lookup s); e' |] "" builder)                                                                               
+                            | A.Mut(A.Float) -> (L.build_call assignMutFloat [| (lookup s); e' |] "" builder    )                                                        
+                            | A.Mut(A.Bool) -> (L.build_call assignMutBool [| (lookup s); e' |] "" builder)
+                            | A.Ref(A.Int) when fst e = A.Mut(A.Int) 
+                            -> (L.build_call assignRefIntMut [| (lookup s); e' |] "" builder)
+                            | A.Ref(A.Int) when fst e = A.Ref(A.Int) 
+                            -> (L.build_call assignRefIntRef [| (lookup s); e' |] "" builder)
+                            | A.Ref(A.Float) when fst e = A.Mut(A.Float) 
+                            -> (L.build_call assignRefFloatMut [| (lookup s); e' |] "" builder)
+                            | A.Ref(A.Float) when fst e = A.Ref(A.Float) 
+                            -> (L.build_call assignRefFloatRef [| (lookup s); e' |] "" builder)
+                            | A.Ref(A.Bool) when fst e = A.Mut(A.Bool) 
+                            -> (L.build_call assignRefBoolMut [| (lookup s); e' |] "" builder)
+                            | A.Ref(A.Bool) when fst e = A.Ref(A.Bool) 
+                            -> (L.build_call assignRefBoolRef [| (lookup s); e' |] "" builder)
+
+                            | _ -> L.build_store e' (lookup s) builder); e'
       | SBinop (e1, op, e2) ->
         let e1' = build_expr builder e1
         and e2' = build_expr builder e2 in
@@ -359,7 +481,7 @@ let translate (globals, functions) =
           "printf" builder
       | SCall (f, args) ->
         let (fdef, fdecl) = StringMap.find f function_decls in
-        let llargs = List.rev (List.map (build_expr builder) (List.rev args)) in
+        let llargs = List.rev (List.map (build_expr_args builder) (List.rev args)) in
         let result = f ^ "_result" in
         L.build_call fdef (Array.of_list llargs) result builder
     in
@@ -436,7 +558,7 @@ let translate (globals, functions) =
     (* Add a return if the last block falls off the end *)
     add_terminal func_builder (L.build_ret (L.const_int i32_t 0))
 
-  in
+in
 
   List.iter build_fnction_body functions;
   the_module
